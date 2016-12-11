7ddf92fcc5e7ac99b1dffea9cea31e227f32ae90
SanjayVarun <sanjay.varun95@gmail.com>
2016-12-11 18:47:03 +0530

    XenonHD Script

---
+++ script_build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# ccache
+export USE_CCACHE=1
+export CCACHE_DIR=/home/ccache/sanjay
+prebuilts/misc/linux-x86/ccache/ccache -M 50G
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export KBUILD_BUILD_USER=SanjayVarun
+export KBUILD_BUILD_HOST=SpeedHorn
+
+# clean
+make clean && make clobber
+
+# build
+. build/envsetup.sh
+lunch xenonhd_$device-userdebug
+make bacon -j8
